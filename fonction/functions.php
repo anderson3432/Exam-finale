be6312aa8edec378dd7f5efab1bc041208ec504e
nyaro <matthewandriamitantsoa@gmail.com>
2026-07-20 09:16:24 +0300

    Update functions.php

--- fonction/functions.php
+++ fonction/functions.php
@@ -40,25 +40,15 @@ function get_membre_by_etu($numero_etu) {
 /**
  * Inscrit automatiquement un membre avec son numéro ETU uniquement
  */
-function inscrire_membre_automatique($numero_etu) {
+function inscrire_membre_automatique($numero_etu,$nom) {
     $link = dbconnect();
-    // On insère juste le numéro ETU, le nom reste vide pour l'instant
-    $sql = "INSERT INTO membre (nom, numero_etu) VALUES ('', '$numero_etu')";
+    $sql = "INSERT INTO membre (nom, numero_etu) VALUES ('$nom', '$numero_etu')";
     
     if (mysqli_query($link, $sql)) {
-        // Renvoie le dernier ID généré (id_membre) pour savoir qui on vient de créer
         return mysqli_insert_id($link);
     }
     return false;
 }
 
-/**
- * Met à jour le nom d'un membre
- */
-function update_nom_membre($id_membre, $nom) {
-    $link = dbconnect();
-    $sql = "UPDATE membre SET nom = '$nom' WHERE id_membre = $id_membre";
-    return mysqli_query($link, $sql);
-}
 
 ?>
